5fc7620a8454ccd2a7bd6c3abca56f865ee74c58
ramramki26 <reachramanan@gmail.com>
2018-03-07 16:08:41 +0530

    Solution for ProgAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Comments on overall working of the two functions:
+## makeCacheMatrix initialises the matrix to be cached and contains 
+## the functions to set the data to be accessed by the cacheSolve func
+## -tion which then manipulates that data to obtain the inverse of the 
+## given matrix either by computing it or retrieving it.
 
-## Write a short comment describing this function
+
+## Comments on makeCacheMatrix:
+## makeCacheMatrix initialises two empty matrices, one is an argument 
+## to be supplied by the user, the other is the inverse. These are
+## manipulated using get,setinverse,getinverse methods by the
+## cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- matrix()
+  set <- function(y) {
+    x <<- y
+    m <<- matrix()
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Comments on cacheSolve:
+## cacheSolve first sets the previous value of inverse. If it was calc
+## -ulated before, the value would be retrieved and the if condition
+## would be true leading to the cached data being printed. If the
+## input is new, the set value would be a matrix of NAs and hence the
+## if condition would be false causing the code to be skipped and the
+## inverse to be computed and then returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+  if(any(!is.na(m))) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
